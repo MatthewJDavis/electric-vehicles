5dce065434e74ab444dc883b574e0af72db278dc
= <matthewdavis111@gmail.com>
2017-02-15 20:38:02 +0000

    update file path vars

--- Azure-Functions/SetBlobToStorageFunction/run.ps1
+++ Azure-Functions/SetBlobToStorageFunction/run.ps1
@@ -2,15 +2,15 @@ Import-Module -Name Azure.Storage
 $sasToken = $($env:sasstring)
 $storageAccountName = $($env:storagename)
 $container = $($env:containername)
-$ctx = New-AzureStorageContext -StorageAccountName $storageAccountName -SasToken $sasToken 
+$ctx = New-AzureStorageContext -StorageAccountName $storageName -SasToken $sasToken 
 $fileName = 'newElectricVehicles.JSON'
-$path = 'D:\home\site\temp'
+$path = 'D:\home\site\temp\'
 
 # create dummy file - this will be json returned from API
-New-Item "$path\test.JSON" -ItemType file -Value "test" -Force
+New-Item "$path$fileName" -ItemType file -Value "test" -Force
 
-$blob = Set-AzureStorageBlobContent -File D:\home\site\temp\test.txt -Container $container -Blob $fileName -BlobType Block -Context $ctx -Force
+$blob = Set-AzureStorageBlobContent -File "$path$fileName" -Container $container -Blob $fileName -BlobType Block -Context $ctx -Force
 
-Remove-Item -Path "$path\test.JSON" -Force
+Remove-Item -Path "$path$fileName" -Force
 
-Out-File -Encoding ascii -FilePath $res -inputObject "Copy and paste the following link into your browser address bar to download the new electric vehicle file: https://$storageAccountName.blob.core.windows.net/$container/$file$sasToken "
+Out-File -Encoding ascii -FilePath $res -inputObject "Copy and paste the following link into your browser address bar to download the new electric vehicle file: https://$storageName.blob.core.windows.net/$container/$fileName$sasToken "

19e80215cdbbe8799c01660bf9eba8228375ee43
= <matthewdavis111@gmail.com>
2017-02-15 19:53:27 +0000

    updated run.ps1 path var

---
+++ Azure-Functions/SetBlobToStorageFunction/run.ps1
@@ -0,0 +1,16 @@
+Import-Module -Name Azure.Storage
+$sasToken = $($env:sasstring)
+$storageName = $($env:storagename)
+$container = $($env:containername)
+$ctx = New-AzureStorageContext -StorageAccountName $storageName -SasToken $sasToken 
+$fileName = 'newElectricVehicles.JSON'
+$path = 'D:\home\site\temp'
+
+# create dummy file - this will be json returned from API
+New-Item "$path\test.JSON" -ItemType file -Value "test" -Force
+
+$blob = Set-AzureStorageBlobContent -File D:\home\site\temp\test.txt -Container $container -Blob $fileName -BlobType Block -Context $ctx -Force
+
+Remove-Item -Path "$path\test.JSON" -Force
+
+Out-File -Encoding ascii -FilePath $res -inputObject "Copy and paste the following link into your browser address bar to download the new electric vehicle file: https://$storageName.blob.core.windows.net/$container/$file$sasToken "

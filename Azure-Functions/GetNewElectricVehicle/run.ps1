dd32873559bb648aa83b028630e52f99138187c6
= <matthewdavis111@gmail.com>
2017-02-20 21:49:27 +0000

    added functions to test api response and upload blob

--- Azure-Functions/GetNewElectricVehicle/run.ps1
+++ Azure-Functions/GetNewElectricVehicle/run.ps1
@@ -22,6 +22,12 @@ if ($req_query_year)
     $year = $year = (Get-Date).Year
 }
 
+#upload file to Azure storage
+function Set-ElecVehicleBlob()
+{    
+    $blob = Set-AzureStorageBlobContent -File "$path$fileName" -Container $container -Blob $fileName -BlobType Block -Context $ctx -Force
+}
+
 function New-ApiQuery($uri) {
     try
     {
@@ -34,8 +40,23 @@ function New-ApiQuery($uri) {
         $apiError += 'Error calling Edumunds API'
         $apiError += $_.Exception.Message
         $apiError += $_.ErrorDetails.Message
+        Return $apiError + $false
     }
 }
+
+# stop the function if there is an error returned and upload details to blob storage
+function Test-ApiResponse($response)
+{
+    if($response[3] -eq $false)
+    {
+        $response[2] | Out-File -FilePath $path$fileName -Append -Force
+        Set-ElecVehicleBlob
+        break
+    }
+}
+
+
+
 
 #create dir on server if not present
 if (-not (Test-Path -Path $path) ){
@@ -44,6 +65,7 @@ if (-not (Test-Path -Path $path) ){
 
 #get all the makes for the year requested
 $makes = New-ApiQuery("https://api.edmunds.com/api/vehicle/v2/makes?state=new&year=$year&view=basic&fmt=json&api_key=$key")
+Test-ApiResponse($makes)
 
 #get all of the styles for the new models- not used because of API call limit of 25 calls per day
 <#
@@ -58,8 +80,9 @@ foreach ($model in $models)
 
 #just get the styles of chevrolet due to API limit, should return 2 electric vehicles
 $styles = New-ApiQuery("https://api.edmunds.com/api/vehicle/v2/chevrolet/models?state=new&year=$year&view=basic&fmt=json&api_key=$key")
+Test-ApiResponse($styles)
 
-#get engine details for styles and check to see if fuel type is electric
+#get engine details for styles and check for an error, if no error check if fuel type is electric and add to file
 foreach ($id in $styles.models.years.styles.id)
 {
     $id = $id.ToString()
@@ -68,12 +91,9 @@ foreach ($id in $styles.models.years.styles.id)
  
     $engine = New-ApiQuery("https://api.edmunds.com/api/vehicle/v2/styles/$id/engines?availability=standard&fmt=json&api_key=$key")
     
-    if ($engine[3] -eq $false)
-    {
-        $engine | Out-File -FilePath $path$errorFileName -Append -Force
-        break
-    }
-    elseif ($engine.engines.fueltype -like "*electric*")
+    Test-ApiResponse($engine)
+
+    if ($engine.engines.fueltype -like "*electric*")
     {
         Write-Output "electric vehicle found"
         $elecVehicle = New-ApiQuery("https://api.edmunds.com/api/vehicle/v2/styles/$id?view=full&fmt=json&api_key=$key")
@@ -81,8 +101,7 @@ foreach ($id in $styles.models.years.styles.id)
     }   
 }
 
-#upload file to Azure storage
-$blob = Set-AzureStorageBlobContent -File "$path$fileName" -Container $container -Blob $fileName -BlobType Block -Context $ctx -Force
+Set-ElecVehicleBlob
 
 Remove-Item -Path "$path$fileName" -Force
 
